ca6361aaedb09ae643bb900138c7059b7e8af648
Sergio Di Fiore <difiores@outlook.com>
2018-08-31 20:09:04 -0300

    Inicial

---
+++ miniV1.0/Controllers/ReceitasController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace miniV1.Controllers
+{
+    public class ReceitasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Mais_receitas()
+        {
+            return View();
+        }
+    }
+}
