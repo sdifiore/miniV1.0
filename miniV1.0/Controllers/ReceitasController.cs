9049da5f7e663d3a08babc49617aa4f793186697
Sergio Di Fiore <difiores@outlook.com>
2018-09-01 09:22:41 -0300

    ReceitasController

--- miniV1.0/Controllers/ReceitasController.cs
+++ miniV1.0/Controllers/ReceitasController.cs
@@ -17,5 +17,159 @@ namespace miniV1.Controllers
         {
             return View();
         }
+
+        public IActionResult Bacalhau_com_requeijao_de_tofu_e_gengibre()
+        {
+            return View();
+        }
+        public IActionResult Bolo_nega_maluca()
+        {
+            return View();
+        }
+
+        public IActionResult Coscous_couve_flor()
+        {
+            return View();
+        }
+
+        public IActionResult Crocante_de_sementes()
+        {
+            return View();
+        }
+
+        public IActionResult Crumble_low_carb_com_legumes()
+        {
+            return View();
+        }
+
+        public IActionResult Escondidinho_de_carne()
+        {
+            return View();
+        }
+
+        public IActionResult Escondidinho_de_frango_fit()
+        {
+            return View();
+        }
+
+        public IActionResult Fondue_fit_de_chocolate()
+        {
+            return View();
+        }
+
+        public IActionResult Fondue_fit_de_queijo()
+        {
+            return View();
+        }
+
+        public IActionResult Kibe_de_ervilha_partida()
+        {
+            return View();
+        }
+
+        public IActionResult Maionese_de_abacate()
+        {
+            return View();
+        }
+
+        public IActionResult Maionese_de_tofu()
+        {
+            return View();
+        }
+
+        public IActionResult Maionese_de_tofu_e_abacate_ao_wassabi()
+        {
+            return View();
+        }
+
+        public IActionResult Mandioqueijo_Queijo_de_mandioca()
+        {
+            return View();
+        }
+
+        public IActionResult Moqueca_de_palmito_pupunha_com_banana_da_terra()
+        {
+            return View();
+        }
+
+        public IActionResult Muffins_de_batata_doce()
+        {
+            return View();
+        }
+
+        public IActionResult Muffins_de_canela()
+        {
+            return View();
+        }
+
+        public IActionResult Nutella_funcional()
+        {
+            return View();
+        }
+
+        public IActionResult Panqueca_de_cacau_sacietogenica()
+        {
+            return View();
+        }
+
+        public IActionResult Panqueca_de_microondas()
+        {
+            return View();
+        }
+
+        public IActionResult Pao_de_batata_doce()
+        {
+            return View();
+        }
+
+        public IActionResult Pao_de_chia()
+        {
+            return View();
+        }
+
+        public IActionResult Pao_de_espinafre()
+        {
+            return View();
+        }
+
+        public IActionResult Pao_de_frigideira()
+        {
+            return View();
+        }
+
+        public IActionResult Pao_sem_gluten_de_milho_de_micro_ondas()
+        {
+            return View();
+        }
+
+        public IActionResult Queijo_vegano()
+        {
+            return View();
+        }
+
+        public IActionResult Smoothie_de_banana_e_maca()
+        {
+            return View();
+        }
+
+        public IActionResult Smoothie_de_cacau()
+        {
+            return View();
+        }
+
+        public IActionResult Torta_de_banana_funcional()
+        {
+            return View();
+        }
+
+        public IActionResult Tortinha_de_cebola_pure_rustico_agridoce()
+        {
+            return View();
+        }
+
+        public IActionResult Tortinhas_de_limao_maracuja_e_morango()
+        {
+            return View();
+        }
     }
 }
