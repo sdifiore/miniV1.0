3e03522e731cd0ce410c348d036ccd0d00c8e29c
Sergio Di Fiore <difiores@outlook.com>
2018-09-06 15:57:13 -0300

    Banners ok

--- miniV1.0/Models/Contato.cs
+++ miniV1.0/Models/Contato.cs
@@ -7,17 +7,13 @@ namespace miniV1.Models
         public int Id { get; set; }
 
         [Required]
-        [StringLength(128)]
         public string Nome { get; set; }
 
         [Required]
-        [StringLength(128)]
         public string Email { get; set; }
 
-        [StringLength(64)]
         public string Telefone { get; set; }
 
-        [StringLength(5120)]
         public string Comentario { get; set; }
     }
 }
